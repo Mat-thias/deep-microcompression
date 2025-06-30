338e889dd7955795d06b2e90b60852e117f3b362
Mat-thias <busoyeom@gmail.com>
2025-06-30 15:25:33 +0100

    Correct the model size function for the model structure

--- development/layers/linear.py
+++ development/layers/linear.py
@@ -106,27 +106,28 @@ class Linear(nn.Linear):
     def get_size_in_bits(self):
         
         # Use pruned or quantized weights if available
-        weight = self.weight_dmc if hasattr(self, "weight_dmc") else self.weight
-        bias = self.bias_dmc if hasattr(self, "bias_dmc") else self.bias
+        # weight = self.weight_dmc if hasattr(self, "weight_dmc") else self.weight
+        # bias = self.bias_dmc if hasattr(self, "bias_dmc") else self.bias
+        weight, bias = self.get_compression_parameters()
 
         size = 0
 
         if getattr(self, "quantization_type", QUANTIZATION_NONE) == QUANTIZATION_NONE:
             size += get_size_in_bits(weight)
             size += get_size_in_bits(bias)
-        else:
-            size += get_size_in_bits(weight, is_packed=True, bitwidth=self.quantization_bitwidth)
-            size += get_size_in_bits(bias, is_packed=True, bitwidth=self.quantization_bitwidth)
+        # else:
+        #     size += get_size_in_bits(weight, is_packed=True, bitwidth=self.quantization_bitwidth)
+        #     size += get_size_in_bits(bias, is_packed=True, bitwidth=self.quantization_bitwidth)
 
-        # Handle different quantization modes
-        if hasattr(self, "quantization_type"):
-            if getattr(self, "quantization_type") == DYNAMIC_QUANTIZATION_PER_TENSOR or getattr(self, "quantization_type") == DYNAMIC_QUANTIZATION_PER_CHANNEL:
-                size += get_size_in_bits(self.weight_scale)
-            elif getattr(self, "quantization_type") == STATIC_QUANTIZATION_PER_TENSOR or getattr(self, "quantization_type") == STATIC_QUANTIZATION_PER_CHANNEL:
-                size += get_size_in_bits(self.input_zero_point)
-                size += get_size_in_bits(self.bias_scale)
-                size += get_size_in_bits(self.output_scale)
-                size += get_size_in_bits(self.output_zero_point)
+        # # Handle different quantization modes
+        # if hasattr(self, "quantization_type"):
+        #     if getattr(self, "quantization_type") == DYNAMIC_QUANTIZATION_PER_TENSOR or getattr(self, "quantization_type") == DYNAMIC_QUANTIZATION_PER_CHANNEL:
+        #         size += get_size_in_bits(self.weight_scale)
+        #     elif getattr(self, "quantization_type") == STATIC_QUANTIZATION_PER_TENSOR or getattr(self, "quantization_type") == STATIC_QUANTIZATION_PER_CHANNEL:
+        #         size += get_size_in_bits(self.input_zero_point)
+        #         size += get_size_in_bits(self.bias_scale)
+        #         size += get_size_in_bits(self.output_scale)
+        #         size += get_size_in_bits(self.output_zero_point)
 
         return size
 
