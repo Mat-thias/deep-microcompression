6b53932cfd58565f715ccae28d32f8ea37495b19
Mat-thias <busoyeom@gmail.com>
2025-07-02 14:39:13 +0100

    the combined header file for all header files

---
+++ deployment/deep_microcompression.h
@@ -0,0 +1,15 @@
+#ifndef DEEP_MICROCOMPRESSION
+#define DEEP_MICROCOMPRESSION
+
+
+#include "models/sequential.h"
+
+#include "layers/activation.h"
+#include "layers/batchnorm.h"
+#include "layers/conv.h"
+#include "layers/flatten.h"
+#include "layers/linear.h"
+#include "layers/pooling.h"
+
+
+#endif // DEEP_MICROCOMPRESSION
